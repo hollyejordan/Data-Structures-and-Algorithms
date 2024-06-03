22d0cb21919856f408da249652330e610980e349
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2024-06-03 18:28:01 +0100

    Created findShortestPaths()

--- Pathfinding Algorithms/Pathfinding Algorithms/main.cpp
+++ Pathfinding Algorithms/Pathfinding Algorithms/main.cpp
@@ -13,55 +13,74 @@ public:
 	}
 
 	// Adds a connection to a neighborbouring node, and the weight of the path
-	void addPath(char nodeToAdd, int weight) {
+	void addPath(Node* nodeToAdd, int weight) {
 
-		neighbours.push_back(pair <char, int> (nodeToAdd, weight));
+		neighbours.push_back(pair <Node*, int> (nodeToAdd, weight));
 	}
-	vector<pair<char, int>> neighbours;
+
+	void findShortestPaths() {
+
+		for (auto neighbour : neighbours) { // For each of the current node's neighbours
+
+			cout << "first neighbour: " << neighbour.first->name << endl;
+			cout << "it's weight is " << neighbour.second << endl;
+			cout << "it's current distance away is " << neighbour.first->distance << endl;
+
+			// If the node's current distance from the start plus the neighbour's weight is shorter than the neighbour's currently found distance
+			if (distance + neighbour.second < neighbour.first->distance) {
+
+				neighbour.first->distance = distance + neighbour.second; // Update the neighbour's distance to the shorter path
+			}
+			cout << "it's new distance is " << neighbour.first->distance << endl;
+			cout << endl;
+		}
+	}
+
+	vector<pair<Node*, int>> neighbours;
 	char name;
 	int distance = 1000;
-};
+}; 
 
 void addPaths(vector<Node> &nodes) {
 
-	nodes[0].addPath('B', 10);
-	nodes[0].addPath('D', 12);
-	nodes[0].addPath('G', 11);
-	nodes[0].addPath('H', 4);
-	nodes[1].addPath('A', 10);
-	nodes[1].addPath('D', 8);
-	nodes[1].addPath('H', 20);
-	nodes[2].addPath('D', 17);
-	nodes[2].addPath('E', 8);
-	nodes[2].addPath('G', 13);
-	nodes[2].addPath('H', 10);
-	nodes[3].addPath('A', 12);
-	nodes[3].addPath('B', 8);
-	nodes[3].addPath('C', 17);
-	nodes[3].addPath('F', 16);
-	nodes[3].addPath('G', 24);
-	nodes[3].addPath('H', 14);
-	nodes[4].addPath('C', 8);
-	nodes[4].addPath('F', 8);
-	nodes[4].addPath('G', 11);
-	nodes[4].addPath('H', 5);
-	nodes[5].addPath('D', 16);
-	nodes[5].addPath('E', 8);
-	nodes[5].addPath('G', 18);
-	nodes[5].addPath('H', 21);
-	nodes[6].addPath('A', 11);
-	nodes[6].addPath('C', 13);
-	nodes[6].addPath('D', 24);
-	nodes[6].addPath('E', 11);
-	nodes[6].addPath('F', 18);
-	nodes[6].addPath('H', 30);
-	nodes[7].addPath('A', 4);
-	nodes[7].addPath('B', 20);
-	nodes[7].addPath('C', 10);
-	nodes[7].addPath('D', 14);
-	nodes[7].addPath('E', 5);
-	nodes[7].addPath('F', 21);
-	nodes[7].addPath('G', 30);
+	nodes[0].addPath(&nodes[1], 10);
+	nodes[0].addPath(&nodes[3], 12);
+	nodes[0].addPath(&nodes[6], 11);
+	nodes[0].addPath(&nodes[7], 4);
+	nodes[1].addPath(&nodes[0], 10);
+	nodes[1].addPath(&nodes[3], 8);
+	nodes[1].addPath(&nodes[7], 20);
+	nodes[2].addPath(&nodes[3], 17);
+	nodes[2].addPath(&nodes[4], 8);
+	nodes[2].addPath(&nodes[6], 13);
+	nodes[2].addPath(&nodes[7], 10);
+	nodes[3].addPath(&nodes[0], 12);
+	nodes[3].addPath(&nodes[1], 8);
+	nodes[3].addPath(&nodes[2], 17);
+	nodes[3].addPath(&nodes[5], 16);
+	nodes[3].addPath(&nodes[6], 24);
+	nodes[3].addPath(&nodes[7], 14);
+	nodes[4].addPath(&nodes[2], 8);
+	nodes[4].addPath(&nodes[5], 8);
+	nodes[4].addPath(&nodes[6], 11);
+	nodes[4].addPath(&nodes[7], 5);
+	nodes[5].addPath(&nodes[3], 16);
+	nodes[5].addPath(&nodes[4], 8);
+	nodes[5].addPath(&nodes[6], 18);
+	nodes[5].addPath(&nodes[7], 21);
+	nodes[6].addPath(&nodes[0], 11);
+	nodes[6].addPath(&nodes[2], 13);
+	nodes[6].addPath(&nodes[3], 24);
+	nodes[6].addPath(&nodes[4], 11);
+	nodes[6].addPath(&nodes[5], 18);
+	nodes[6].addPath(&nodes[7], 30);
+	nodes[7].addPath(&nodes[0], 4);
+	nodes[7].addPath(&nodes[1], 20);
+	nodes[7].addPath(&nodes[2], 10);
+	nodes[7].addPath(&nodes[3], 14);
+	nodes[7].addPath(&nodes[4], 5);
+	nodes[7].addPath(&nodes[5], 21);
+	nodes[7].addPath(&nodes[6], 30);
 }
 
 
@@ -72,6 +91,8 @@ int main() {
 	addPaths(nodes);
 
 	nodes[0].distance = 0;
+
+	nodes[0].findShortestPaths();
 
 	return 0;
 }
