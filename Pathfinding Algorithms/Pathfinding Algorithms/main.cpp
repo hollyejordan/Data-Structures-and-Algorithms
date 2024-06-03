82353f04fb1e2ee0a083f06cc3677dcd6974bbd3
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2024-06-04 00:06:58 +0100

    Completed Djikstra finding the shortest distance to all nodes

--- Pathfinding Algorithms/Pathfinding Algorithms/main.cpp
+++ Pathfinding Algorithms/Pathfinding Algorithms/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <queue>
 using namespace std;
 
 class Node {
@@ -15,33 +16,38 @@ public:
 	// Adds a connection to a neighborbouring node, and the weight of the path
 	void addPath(Node* nodeToAdd, int weight) {
 
-		neighbours.push_back(pair <Node*, int> (nodeToAdd, weight));
+		neighbours.push_back(pair <Node*, int>(nodeToAdd, weight));
 	}
 
-	void findShortestPaths() {
+	vector<Node*> findShortestPaths() {
+
+		vector<Node*> nodesToAddToQueue;
 
 		for (auto neighbour : neighbours) { // For each of the current node's neighbours
-
-			cout << "first neighbour: " << neighbour.first->name << endl;
-			cout << "it's weight is " << neighbour.second << endl;
-			cout << "it's current distance away is " << neighbour.first->distance << endl;
 
 			// If the node's current distance from the start plus the neighbour's weight is shorter than the neighbour's currently found distance
 			if (distance + neighbour.second < neighbour.first->distance) {
 
 				neighbour.first->distance = distance + neighbour.second; // Update the neighbour's distance to the shorter path
-			}
-			cout << "it's new distance is " << neighbour.first->distance << endl;
-			cout << endl;
-		}
-	}
 
+				if (!neighbour.first->visited) { // If neightbour has not been visited yet
+
+					nodesToAddToQueue.push_back(neighbour.first); // Get ready to add it to the queue
+				}
+			}
+		}
+		visited = true; // The current node has now been visited
+
+		return nodesToAddToQueue;
+	}
 	vector<pair<Node*, int>> neighbours;
 	char name;
 	int distance = 1000;
-}; 
+	bool visited = false;
+};
 
-void addPaths(vector<Node> &nodes) {
+
+void addPaths(vector<Node>& nodes) {
 
 	nodes[0].addPath(&nodes[1], 10);
 	nodes[0].addPath(&nodes[3], 12);
@@ -83,16 +89,45 @@ void addPaths(vector<Node> &nodes) {
 	nodes[7].addPath(&nodes[6], 30);
 }
 
-
 int main() {
 
 	vector<Node> nodes = { {'A'}, {'B'}, {'C'}, {'D'}, {'E'}, {'F'}, {'G'}, {'H'}};
-	
 	addPaths(nodes);
 
-	nodes[0].distance = 0;
+	auto cmpDistance = [](Node* a, Node* b) { return a->distance > b->distance; };
+	priority_queue<Node*, vector<Node*>, decltype(cmpDistance)> queue(cmpDistance);
 
-	nodes[0].findShortestPaths();
+	nodes[0].distance = 0; // Set the starting node's distance to zero
+	queue.push(&nodes[0]);
+	vector<Node*> nodesToAddToQueue;
+
+	while (!queue.empty()) {
+
+		nodesToAddToQueue = queue.top()->findShortestPaths();
+		cout << queue.top()->name << " " << queue.top()->distance << endl;
+		queue.pop();
+
+		for (auto &node : nodesToAddToQueue) {
+
+			queue.push(node);
+		}
+	}
+	
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+	/*
+	cout << nodes[0].name << " " << nodes[0].distance << endl;
+	cout << nodes[1].name << " " << nodes[1].distance << endl;
+	cout << nodes[2].name << " " << nodes[2].distance << endl;
+	cout << nodes[3].name << " " << nodes[3].distance << endl;
+	cout << nodes[4].name << " " << nodes[4].distance << endl;
+	cout << nodes[5].name << " " << nodes[5].distance << endl;
+	cout << nodes[6].name << " " << nodes[6].distance << endl;
+	cout << nodes[7].name << " " << nodes[7].distance << endl;
+	*/
 
 	return 0;
 }
